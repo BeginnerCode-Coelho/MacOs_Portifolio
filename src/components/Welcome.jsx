8e50a1d3ce4d33873f4eb4ca91e20a9c7e3aa1fb
Your Name <you@example.com>
2026-04-24 18:37:15 -0300

    implement dock presentation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { useGSAP }  from "@gsap/react";
 gsap.registerPlugin(useGSAP);
 
 const FONT_WEIGHTs = {
